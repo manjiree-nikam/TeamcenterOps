46cd5c7ab71fedefd4e08ef791bcc97b92df5ec6
manjiree-nikam <manjiree.nikam@gmx.de>
2026-09-18 21:36:34 +0200

    Teamcenter login

---
+++ src/Tc_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <tc/tc.h>
+#include <tc/emh.h>
+
+int main(int argc, char *argv[])
+{
+    int ifail = ITK_ok;
+
+    printf("Teamcenter ITK Test Program\n");
+
+    ifail = ITK_init_module("infodba", "infodba", "dba");
+
+    if (ifail != ITK_ok)
+    {
+        printf("Teamcenter login failed.\n");
+        return ifail;
+    }
+
+    printf("Teamcenter login successful.\n");
+
+    ITK_exit_module(TRUE);
+
+    return ITK_ok;
+}
